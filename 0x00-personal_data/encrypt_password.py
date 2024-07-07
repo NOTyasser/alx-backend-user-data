263dd6c16f24bcc1d9539029c53d5136084bc186
NOTyasser <125504642+NOTyasser@users.noreply.github.com>
2024-07-07 02:58:23 +0200

    all

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-"""Encrypting passwords
-Check valid password"""
-
+"""
+Encrypting passwords
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """returns a salted, hashed password,
-    which is a byte string."""
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    """ Returns a salted, hashed password, which is a byte string """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """checks if a pswrd is valid"""
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    """ Validates the provided password matches the hashed password """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
